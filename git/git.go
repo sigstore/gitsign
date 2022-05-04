acfa443adff97b9691775611334bdc5beffa5df4
Billy Lynch <billy@chainguard.dev>
2022-05-04 12:45:53 -0400

    Refactor rekor bits into their own library.
    Also adds public key validation. Wanted to add unit tests here, but
    working with Rekor without a real instance proves to be difficult, since
    many of the libraries require validation of hashes.
    
    Signed-off-by: Billy Lynch <billy@chainguard.dev>

--- git/git.go
+++ git/git.go
@@ -3,27 +3,18 @@ package git
 import (
 	"bytes"
 	"context"
-	"crypto/sha256"
 	"crypto/x509"
-	"encoding/hex"
 	"encoding/pem"
-	"fmt"
-	"strings"
 
 	cms "github.com/github/smimesign/ietf-cms"
 	"github.com/github/smimesign/signature"
 	"github.com/go-git/go-git/v5/plumbing"
 	"github.com/go-git/go-git/v5/plumbing/object"
-	"github.com/go-openapi/strfmt"
-	"github.com/go-openapi/swag"
 	"github.com/pkg/errors"
 	"github.com/sigstore/cosign/cmd/cosign/cli/fulcio/fulcioroots"
-	"github.com/sigstore/cosign/cmd/cosign/cli/rekor"
-	"github.com/sigstore/cosign/pkg/cosign"
-	"github.com/sigstore/rekor/pkg/generated/client"
-	"github.com/sigstore/rekor/pkg/generated/client/index"
 	"github.com/sigstore/rekor/pkg/generated/models"
 	"github.com/wlynch/smimecosign/fulcio"
+	"github.com/wlynch/smimecosign/rekor"
 )
 
 func Sign(ctx context.Context, ident *fulcio.Identity, data []byte, opts signature.SignOptions) ([]byte, *x509.Certificate, error) {
@@ -38,7 +29,7 @@ func Sign(ctx context.Context, ident *fulcio.Identity, data []byte, opts signatu
 	// using the same key, this is probably okay? e.g. even if you could cause a SHA1 collision,
 	// you would still need the underlying commit to be valid and using the same key which seems hard.
 
-	rClient, err := rekor.NewClient("https://rekor.sigstore.dev")
+	rekor, err := rekor.New("https://rekor.sigstore.dev")
 	if err != nil {
 		return nil, nil, errors.Wrap(err, "error creating rekor client")
 	}
@@ -53,9 +44,8 @@ func Sign(ctx context.Context, ident *fulcio.Identity, data []byte, opts signatu
 	if err != nil {
 		return nil, nil, errors.Wrap(err, "error signing commit hash")
 	}
-	if _, err := cosign.TLogUpload(ctx, rClient, commitSig, []byte(commit), sv.Cert); err != nil {
+	if _, err := rekor.Write(ctx, commitSig, []byte(commit), sv.Cert); err != nil {
 		return nil, nil, errors.Wrap(err, "error uploading tlog (commit)")
-
 	}
 
 	return sig, cert, nil
@@ -139,11 +129,22 @@ func Verify(ctx context.Context, data, sig []byte) (*VerificationSummary, error)
 		return nil, err
 	}
 
-	tlog, err := verifyTlog(ctx, commit, certs[0])
+	rekor, err := rekor.New("https://rekor.sigstore.dev")
 	if err != nil {
+		claims = append(claims, NewClaim(ClaimLocatedRekorEntry, false))
 		return nil, err
 	}
+	tlog, err := rekor.Get(ctx, commit, certs[0])
+	if err != nil {
+		claims = append(claims, NewClaim(ClaimLocatedRekorEntry, false))
+		return nil, errors.Wrap(err, "failed to locate rekor entry")
+	}
 	claims = append(claims, NewClaim(ClaimLocatedRekorEntry, true))
+
+	if err := rekor.Verify(ctx, tlog); err != nil {
+		claims = append(claims, NewClaim(ClaimValidatedRekorEntry, true))
+		return nil, errors.Wrap(err, "failed to validate rekor entry")
+	}
 	claims = append(claims, NewClaim(ClaimValidatedRekorEntry, true))
 
 	return &VerificationSummary{
@@ -151,62 +152,6 @@ func Verify(ctx context.Context, data, sig []byte) (*VerificationSummary, error)
 		LogEntry: tlog,
 		Claims:   claims,
 	}, nil
-}
-
-func verifyTlog(ctx context.Context, commit string, cert *x509.Certificate) (*models.LogEntryAnon, error) {
-	rClient, err := rekor.NewClient("https://rekor.sigstore.dev")
-	if err != nil {
-		return nil, err
-	}
-
-	pk, err := publicKeyFromCert(cert)
-	if err != nil {
-		return nil, err
-	}
-	uuids, err := findTLogEntriesByPayloadAndPK(ctx, rClient, []byte(commit), pk)
-	if err != nil {
-		return nil, err
-	}
-
-	for _, u := range uuids {
-		e, err := cosign.GetTlogEntry(ctx, rClient, u)
-		if err != nil {
-			return nil, err
-		}
-
-		if err := cosign.VerifyTLogEntry(ctx, rClient, e); err != nil {
-			continue
-		}
-
-		// TODO: don't trust rekor response - verify client-side that the
-		// public key matches.
-
-		return e, nil
-	}
-
-	return nil, errors.New("could not find matching tlog entry")
-}
-
-// findTLogEntriesByPayloadAndPK is roughly equivalent to cosign.FindTLogEntriesByPayload,
-// but also filters by the public key used.
-func findTLogEntriesByPayloadAndPK(ctx context.Context, rekorClient *client.Rekor, payload, pubKey []byte) (uuids []string, err error) {
-	params := index.NewSearchIndexParamsWithContext(ctx)
-	params.Query = &models.SearchIndex{}
-
-	h := sha256.New()
-	h.Write(payload)
-	params.Query.Hash = fmt.Sprintf("sha256:%s", strings.ToLower(hex.EncodeToString(h.Sum(nil))))
-
-	params.Query.PublicKey = &models.SearchIndexPublicKey{
-		Content: strfmt.Base64(pubKey),
-		Format:  swag.String(models.SearchIndexPublicKeyFormatX509),
-	}
-
-	searchIndex, err := rekorClient.Index.SearchIndex(params)
-	if err != nil {
-		return nil, err
-	}
-	return searchIndex.GetPayload(), nil
 }
 
 func commitHash(data, sig []byte) (string, error) {
@@ -230,15 +175,4 @@ func commitHash(data, sig []byte) (string, error) {
 	out := &plumbing.MemoryObject{}
 	err := c.Encode(out)
 	return out.Hash().String(), err
-}
-
-func publicKeyFromCert(cert *x509.Certificate) ([]byte, error) {
-	pk, err := x509.MarshalPKIXPublicKey(cert.PublicKey)
-	if err != nil {
-		return nil, errors.Wrap(err, "error marshalling public key")
-	}
-	return pem.EncodeToMemory(&pem.Block{
-		Type:  "PUBLIC KEY",
-		Bytes: pk,
-	}), nil
 }
