29836f4933a56b9a54474f251288ce055ca59204
Billy Lynch <billy@chainguard.dev>
2022-11-21 13:14:53 -0500

    Add CI test for checking CLI doc freshness.
    Signed-off-by: Billy Lynch <billy@chainguard.dev>

--- docs/cli/main.go
+++ docs/cli/main.go
@@ -16,15 +16,33 @@
 package main
 
 import (
-	"log"
+	"fmt"
+	"os"
 
 	"github.com/sigstore/gitsign/internal/commands/root"
+	"github.com/spf13/cobra"
 	"github.com/spf13/cobra/doc"
 )
 
+var (
+	dir string
+	cmd = &cobra.Command{
+		Use:   "gendoc",
+		Short: "Generate help docs",
+		Args:  cobra.NoArgs,
+		RunE: func(*cobra.Command, []string) error {
+			return doc.GenMarkdownTree(root.New(nil), dir)
+		},
+	}
+)
+
+func init() {
+	cmd.Flags().StringVarP(&dir, "dir", "d", ".", "Path to directory in which to generate docs")
+}
+
 func main() {
-	err := doc.GenMarkdownTree(root.New(nil), ".")
-	if err != nil {
-		log.Fatal(err)
+	if err := cmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
 	}
 }
